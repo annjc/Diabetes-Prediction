242db33bc7bc2ab78f9a0d8da1fe258d90464ec0
Ann <49486372+annjezc@users.noreply.github.com>
2020-05-14 22:24:33 +0530

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -1,3 +1,4 @@
+#importing the libraries
 import numpy as np
 import pandas as pd
 import scipy.stats
@@ -5,8 +6,9 @@ from sklearn import model_selection
 from sklearn.cluster import KMeans
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.decomposition import PCA
+#importing the dataset
 df=pd.read_csv("diabetes.csv")
 #Replacing 0 to NaN
 df[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = df[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0, np.NaN)
@@ -19,27 +21,27 @@ x = df.drop(['Outcome'], axis=1)
 #z-score normalization
 z=scipy.stats.zscore(x)
 z=pd.DataFrame(z)
-#Applying PCA
-pca = PCA(n_components=.95)
-pca.fit(z)
-x_pca = pca.transform(z)
 #Applying k-means
 kmeans = KMeans(n_clusters=2,random_state=42)
-kmeans.fit(x_pca)
-y_kmeans = kmeans.predict(x_pca)
+kmeans.fit(z)
+y_kmeans = kmeans.predict(z)
 x['Outcome']=y_kmeans
 #Extracting the correctly classified data
 df['OutcomeMatch'] = np.where(df.Outcome == x.Outcome, 'True', 'False')
 df.drop(df[df['OutcomeMatch'] == "False"].index, inplace = True)
 df=df.drop(['OutcomeMatch'],axis=1)
+print("The no. of correctly classified data after k-means clustering:",len(df.index))
 x = df.drop('Outcome',axis=1)
 y = df['Outcome']
 #Logistic Regression k-fold cross validation
+scaler = StandardScaler() 
+x = scaler.fit_transform(x)
+x=pd.DataFrame(x)
 kfold = model_selection.KFold(n_splits=10)
 model = LogisticRegression()
-
 y_pred = cross_val_predict(model, x, y, cv=10)
 conf_mat = confusion_matrix(y, y_pred)
-results = model_selection.cross_val_score(model, x, y, cv=10)
-print("Accuracy: %.3f%%" % (results.mean()*100.0))
+results = model_selection.cross_val_score(model, x, y, cv=10, scoring='accuracy')
+print("Accuracy of the model: %.3f%%" % (results.mean()*100.0))
+print("The confusion matrix")
 print(conf_mat)
